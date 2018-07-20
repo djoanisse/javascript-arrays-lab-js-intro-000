166c630b814e6fcf314f2840c5336687640db278
joanissedan@gmail.com <joanissedan@gmail.com>
2018-07-20 20:25:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your functions and code here
+var kittens = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
+
+var kittens = ["Milo", "Otis", "Garfield"];
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+var kittens = ["Milo", "Otis", "Garfield"];
+  function destructivelyRemoveLastKitten(name) {
+  kittens.pop(2);
+}
+
+var kittens = ["Milo", "Otis", "Garfield"];
+  function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(1);
+}
+
+//var kittens = ["Milo", "Otis", "Garfield"];
+function appendKitten(name) {
+  var moreKittens = kittens.push(name);
+  return moreKittens;
+}
+
+var kittens = ["Milo", "Otis", "Garfield"];
+  function prependKitten(name) {
+  kittens
+}
