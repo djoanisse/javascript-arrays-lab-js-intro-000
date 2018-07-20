2b351ee6d96183a6d88954e019681f59a17272d7
joanissedan@gmail.com <joanissedan@gmail.com>
2018-07-20 21:48:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,6 +27,6 @@ var kittens = ["Milo", "Otis", "Garfield"];
 
 var kittens = ["Milo", "Otis", "Garfield"];
   function prependKitten(name) {
-    var moreKittens = kittens.splice(0, 0, name);
-    return moreKittens;
+  var moreKittens = kittens.splice(0, 0, name);
+  return moreKittens;
 }
